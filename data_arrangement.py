1f4c40a15c9d3914f312239ee2b7c0175f2f2f60
nicolasmoraistl <nicolas.moraistl@gmail.com>
2026-06-26 20:07:27 -0300

    Futher progress in data transform

--- data_arrangement.py
+++ data_arrangement.py
@@ -1,19 +1,24 @@
 #o tratamento de dados será feito de modo à facilitar a análise usando o R
 #biblioteca do pandas importada para dataframe
+
 import pandas as pd
+from pandas import set_option
 
 #trazer base csv
 database_main = pd.read_csv('data_frame_en.csv', sep=',')
 
-#algumas bases possuem dados indesejáveis, vamos criar uma função para eliminar esses erros.
-#para os dados extraídos no World Bank que possuem o mesmo formato
-def filter_years(dataframe):
-  years = [str(year) for year in range(1995, 2025)]
-  return dataframe[['Country Name'] + years]
+database_main['corruption_control'] = pd.NA
+
+
+#criar função que transfira os dados
 
 def transfer_data_to_main(dataframe, variable, database_main):
+  
     # Filtrando anos de 1995 a 2024
-    df_year_filtered = filter_years(dataframe)
+    
+    year_cols = [col for col in dataframe.columns if col.isdigit()]
+    
+    df_year_filtered = dataframe[['Country Name'] + year_cols].copy()
     
     # Transferindo os anos para o formato longo usando o método melt
     df_long_format = df_year_filtered.melt(
@@ -100,5 +105,77 @@ database_main.to_csv('main_database.csv', index=False)
 fuel_exports = pd.read_csv('data/%fuel exports-merchandise/API_TX.VAL.FUEL.ZS.UN_DS2_en_csv_v2_1353.csv')
 
 database_main = transfer_data_to_main(fuel_exports, 'fuel_share_exports', database_main)
+
+database_main.to_csv('main_database.csv', index=False)
+
+#índice de desenvolvimento humano.
+#esse arquivo mistura diversos índices
+#iremos extrair somente o hdi referente aos países do data set principal
+
+hdi_index = pd.read_csv('data/HDI/hdr_data.csv')
+
+hdi_index = hdi_index[['country', 'indicator', 'year', 'value']]
+
+#filtro do que queremos
+hdi_index = hdi_index[
+    (hdi_index['indicator'] == 'Human Development Index (value)') & 
+    (hdi_index['country'].isin(database_main['countries']))
+  ]
+
+#renomear colunas para corresponderem
+hdi_index = hdi_index.rename(columns={'country': 'countries'})
+
+#fazer merge
+database_main = database_main.merge(
+    hdi_index[['countries', 'year', 'value']], 
+    on=['countries', 'year'], 
+    how='left'
+)
+
+database_main['hdi'] = database_main['value']
+database_main = database_main.drop('value', axis=1)
+
+database_main.to_csv('main_database.csv', index=False)
+
+
+#índices de eficiência governamental
+
+gov_eff = pd.read_csv('data/governement_effectiveness_indexes/3be8611c-a998-4ed7-ad29-16f485b87ebf_Data.csv')
+
+#mudar nome de colunas
+
+columns = list(gov_eff.columns)
+
+for column in columns[4:]:
+  gov_eff = gov_eff.rename({column:column[0:4]}, axis=1)
+  
+#podemos reutilizar a função de transferência de dados ao cortar em 4 variáveis.
+
+df_corruption = gov_eff[gov_eff['Series Code'] == 'GOV_WGI_CC.SC']
+df_stability = gov_eff[gov_eff['Series Code'] == 'GOV_WGI_PV.SC']
+df_effectiveness = gov_eff[gov_eff['Series Code'] == 'GOV_WGI_GE.SC']
+df_rule_of_law = gov_eff[gov_eff['Series Code'] == 'GOV_WGI_RL.SC']
+
+
+database_main = transfer_data_to_main(df_corruption, 'corruption_control', database_main)
+database_main = transfer_data_to_main(df_stability, 'gov_stability', database_main)
+database_main = transfer_data_to_main(df_effectiveness, 'bureaucratic_eff', database_main)
+database_main = transfer_data_to_main(df_rule_of_law, 'rule_of_law', database_main)
+
+database_main.to_csv('main_database.csv', index=False)
+
+#checar se é exportador de combustíveis e rico em recursos
+df_resources = pd.read_csv('data/development-revenue/world-imf2026.csv')
+
+df_resources = df_resources[df_resources['year'] > 1994]
+
+df_imf_selected = df_resources[['country', 'year', 'FuelExporting', 'ResourceRich']]
+
+database_main = database_main.merge(
+    df_imf_selected,
+    left_on=['countries', 'year'], 
+    right_on=['country', 'year'],    
+    how='left'                       
+)
 
 database_main.to_csv('main_database.csv', index=False)
